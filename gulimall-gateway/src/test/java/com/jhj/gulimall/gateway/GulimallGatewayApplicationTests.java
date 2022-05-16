e394433c6038eb633d1a9618f67fb220b4fb40f5
jinhaojie <hnjzjhj@163.com>
2022-05-16 17:35:42 +0800

    1

---
+++ gulimall-gateway/src/test/java/com/jhj/gulimall/gateway/GulimallGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jhj.gulimall.gateway;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
