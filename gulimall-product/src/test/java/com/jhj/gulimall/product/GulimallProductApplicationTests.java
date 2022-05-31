3e80b06482c3ef6a2579de04f9df2b3844cccf57
jinhaojie <hnjzjhj@163.com>
2022-05-31 18:03:08 +0800

    1

--- gulimall-product/src/test/java/com/jhj/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/jhj/gulimall/product/GulimallProductApplicationTests.java
@@ -1,15 +1,18 @@
 package com.jhj.gulimall.product;
 
+
 import com.jhj.gulimall.product.entity.BrandEntity;
 import com.jhj.gulimall.product.service.BrandService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class GulimallProductApplicationTests {
     @Autowired
     BrandService brandService;
+
     @Test
     void contextLoads() {
         BrandEntity brandEntity = new BrandEntity();
@@ -18,5 +21,8 @@ class GulimallProductApplicationTests {
         brandService.updateById(brandEntity);
         System.out.println("保存成功");
     }
+
+
+
 
 }
