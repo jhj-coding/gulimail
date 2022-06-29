5b2209e22f9d2e2a93ea39948eeffe0a0c7b90dc
jinhaojie <hnjzjhj@163.com>
2022-06-29 18:01:27 +0800

    1

--- gulimall-product/src/test/java/com/jhj/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/jhj/gulimall/product/GulimallProductApplicationTests.java
@@ -1,27 +1,42 @@
 package com.jhj.gulimall.product;
 
 
-import com.jhj.gulimall.product.entity.BrandEntity;
 import com.jhj.gulimall.product.service.BrandService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.UUID;
 
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class GulimallProductApplicationTests {
     @Autowired
     BrandService brandService;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     @Test
-    void contextLoads() {
-        BrandEntity brandEntity = new BrandEntity();
-        brandEntity.setBrandId(1L);
-        brandEntity.setDescript("华为");
-        brandService.updateById(brandEntity);
-        System.out.println("保存成功");
+    public void contextLoads() {
+        System.out.println(1);
     }
 
+
+    @Test
+    public void redisTest() {
+        ValueOperations<String, String> stringStringValueOperations = stringRedisTemplate.opsForValue();
+        //保存
+        stringStringValueOperations.set("hello","word_"+ UUID.randomUUID());
+        //查询
+        System.out.println(stringStringValueOperations.get("hello"));
+    }
 
 
 
